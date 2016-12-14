4bfb4eba2eb1735065b388d4fab4c0ee32ee3fc9
gdmec07150834 <727526881@qq.com>
2016-12-14 09:23:15 +0800

    aaa

--- app/src/main/java/cn/edu/gdmec/s07150834/mycontacts/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/s07150834/mycontacts/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    //声明界面控件属性
+
     private EditText nameEditText;
     private EditText mobileEditText;
     private EditText qqEditText;
@@ -20,14 +20,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("添加联系人");
-        //获取界面控件实例
+
         nameEditText = (EditText) findViewById(R.id.name);
         mobileEditText= (EditText) findViewById(R.id.mobile);
         qqEditText= (EditText) findViewById(R.id.qq);
         danweiEditText= (EditText) findViewById(R.id.danwei);
         addressEditText= (EditText) findViewById(R.id.address);
     }
-    //创建选项菜单
+
 
 
     @Override
@@ -36,24 +36,24 @@ public class MainActivity extends AppCompatActivity {
         menu.add(Menu.NONE,2,Menu.NONE,"返回");
         return super.onCreateOptionsMenu(menu);
     }
-    //选项菜单点击事件
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case 1:
                 if (!nameEditText.getText().toString().equals("")){
-                    //新建User对象
+
                     User user=new User();
-                    //User对象赋值
+
                     user.setName(nameEditText.getText().toString());
                     user.setMobile(mobileEditText.getText().toString());
                     user.setDanwei(danweiEditText.getText().toString());
                     user.setQq(qqEditText.getText().toString());
                     user.setAddress(addressEditText.getText().toString());
-                    //创建数据表对象
+
                     ContactsTable ct =new ContactsTable(MainActivity.this);
-                    //数据表增加数据
+
                     if (ct.addData(user)){
                         Toast.makeText(MainActivity.this,"添加成功",Toast.LENGTH_SHORT).show();
 
